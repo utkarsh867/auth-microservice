dadfa52e8efe86892b1d4322618e23e4e6c9653f
utkarsh867 <utkarsh867@gmail.com>
2019-02-11 19:24:03 +0800

    Added docker compose

--- hello/test/hello.test.js
+++ hello/test/hello.test.js
@@ -1,47 +1,49 @@
-const hello = require('../routes/hello');
-const jwt = require('jsonwebtoken');
-const CONFIG = require('../config');
+const hello = require("../routes/hello");
+const jwt = require("jsonwebtoken");
 
-test('User makes a request with a JWT token with Bearer method', done => {
+test("User makes a request with a JWT token with Bearer method", done => {
 	const token = jwt.sign(
 		{
-			username: 'admin',
-			password: 'admin'
+			username: "admin",
+			password: "admin"
 		},
-		CONFIG.JWTSECRET,
+		process.env.JWTSECRET,
 		{ expiresIn: 120 }
 	);
-	const response = hello.VerifyJWT({
-		headers: {
-			authorization: `Bearer ${token}`
-		}
-	});
+	hello
+		.VerifyJWT({
+			headers: {
+				authorization: `Bearer ${token}`
+			}
+		})
+		.then(response => {
+			expect(response).toBeDefined();
+			expect(response.status).toEqual(200);
+			expect(response.message).toEqual("Hello admin");
 
-	expect(response).toBeDefined();
-	expect(response.status).toEqual(200);
-	expect(response.message).toEqual('Hello admin');
-
-	done();
+			done();
+		});
 });
 
-test('User makes a request with a JWT token with invalid method', done => {
+test("User makes a request with a JWT token with invalid method", done => {
 	const token = jwt.sign(
 		{
-			username: 'admin',
-			password: 'admin'
+			username: "admin",
+			password: "admin"
 		},
-		CONFIG.JWTSECRET,
+		process.env.JWTSECRET,
 		{ expiresIn: 120 }
 	);
-	const response = hello.VerifyJWT({
-		headers: {
-			authorization: token
-		}
-	});
+	hello
+		.VerifyJWT({
+			headers: {
+				authorization: token
+			}
+		})
+		.then(response => {
+			expect(response).toBeDefined();
+			expect(response.status).toEqual(401);
 
-	expect(response).toBeDefined();
-	expect(response.status).toEqual(401);
-
-	done();
+			done();
+		});
 });
-
